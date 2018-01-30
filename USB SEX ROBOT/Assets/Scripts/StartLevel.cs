f3dedee4a90dca21058c746c4143d435c80c514e
moun3m <112365@edu.nhtv.nl>
2018-01-31 00:55:30 +0100

    fix sound + cleaned up menu
    remove unused assets in menu
    sound plays when starting game

--- USB SEX ROBOT/Assets/Scripts/StartLevel.cs
+++ USB SEX ROBOT/Assets/Scripts/StartLevel.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 
 public class StartLevel : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		//SceneManager.sceneLoaded
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public AudioClip geluitje;
+    private AudioSource saus;
 
-        if(Input.anyKeyDown)
-            SceneManager.LoadScene("FinalLevel",LoadSceneMode.Single);
+    // Use this for initialization
+    void Start () {
+        //SceneManager.sceneLoaded
+        saus = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (Input.anyKeyDown) { 
+        saus.PlayOneShot(geluitje);
+      //  Debug.Log("Bevroren kontjes");
+      //  Debug.Log(Input.anyKeyDown);
+        SceneManager.LoadScene("FinalLevel", LoadSceneMode.Single);
+    }
 
     }
 }

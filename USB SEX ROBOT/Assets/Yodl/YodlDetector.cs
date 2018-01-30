c4ab0a9e4e5570d93517cd0f8baa82d6030df5ee
xArcky <keanownage@gmail.com>
2018-01-31 00:56:16 +0100

    Added Yodl UI pop-ups when running over people
    Added Yodl (characters panicking when almost run over) and start of GameStateManager for upcoming Intro/Outro

---
+++ USB SEX ROBOT/Assets/Yodl/YodlDetector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YodlDetector : MonoBehaviour
+{
+    public GameObject yodlNode;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PerpDescription>())
+        {
+            if (!other.GetComponentInChildren<YodlGenerator>())
+            {
+                Debug.LogWarning("Colliding!");
+
+                Transform colOrigin = other.transform;
+                GameObject obj = Instantiate(yodlNode, colOrigin);
+
+                obj.transform.parent = other.transform;
+            }
+        }
+    }
+}

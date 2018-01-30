1f10b1e00d2422a8b7f6d29e9a9a5f8bb2c4d4f6
moun3m <112365@edu.nhtv.nl>
2018-01-30 21:50:35 +0100

    menu fix
    ja is gewoon mooi gvd

---
+++ USB SEX ROBOT/Assets/spin2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class spin2 : MonoBehaviour
+{
+    public float speed2 = 100f;
+
+
+    void Update()
+    {
+        transform.Rotate(Vector3.right, speed2 * Time.deltaTime);
+    }
+}

2d15e5a39a0a3b87d4d8e0a43d24d5a034a16ffb
xArcky <keanownage@gmail.com>
2018-02-01 00:53:50 +0100

    Added camera zoomout after game is completed
    Added camera zoomout after game is completed - loads level after zooming out a lot

--- USB SEX ROBOT/Assets/GameStateManager.cs
+++ USB SEX ROBOT/Assets/GameStateManager.cs
@@ -6,26 +6,72 @@ using UnityEngine;
 public class GameStateManager : MonoBehaviour {
 
     [SerializeField]
-    Camera playerCam;
+    GameObject playerCam;
 
     [SerializeField]
-    Camera endCam;
+    GameObject endCam;
 
+    [SerializeField]
+    GameObject endScreenCar;
 
-	// Use this for initialization
-	void Start () {
-		
+    [HideInInspector]
+    public bool sequenceStarted;
+
+    // Use this for initialization
+    void Start ()
+    {
+        sequenceStarted = false;	
 	}
+
+    /* /// DEBUG FOR END SEQUENCE TESTING PURPOSES
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            StartEndSequence();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            playerCam.SetActive(true);
+            endCam.SetActive(false);
+        }
+    }
+    */
+
 
     // ENDSEQUENCE
     public void StartEndSequence()
     {
+        if (sequenceStarted == false)
+        {
+        // Switch to Camera parented above player that zooms out slowly
+        playerCam.SetActive(false);
+        endCam.SetActive(true);
+
+        // Do  model swap to Trumpede car since player model isn't actual model
+        GameObject car = Instantiate(endScreenCar);
+        car.transform.parent = gameObject.transform;
+        car.transform.localPosition = new Vector3(0,0,0);
+
+        Quaternion rotationToSet = gameObject.transform.localRotation;
+        car.transform.localRotation = Quaternion.identity;
+
+        // Play zoomout animation
+        Animator anim = endCam.GetComponent<Animator>();
+        anim.SetBool("ZoomOut", true);
+
+        StartCoroutine(DelayUntilRestart());
+
+            //bool startedZoom? = Animator.StringToHash("ZoomOut");
+            //anim.StringToHash(startedZoom);
+        }
+    }
+
+    IEnumerator DelayUntilRestart()
+    {
+        yield return new WaitForSeconds(10);
         SceneManager.LoadScene("menu", LoadSceneMode.Single);
-
-        //Switch to Camera parented above player that zooms out slowly
-        //playerCam.enabled = false;
-        //endCam.enabled = true;
-        // Do fucking model swap to Trumpede car (taken from Mounim's main menu)
-
     }
 }

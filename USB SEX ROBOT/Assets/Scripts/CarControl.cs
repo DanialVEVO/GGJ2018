0c6f62d0dd47f8afdc5cfdd241a5cc1adf95736b
MarijnvanKrieken <m.vankrieken@hotmail.com>
2018-01-31 18:08:23 +0100

    looking sideways

--- USB SEX ROBOT/Assets/Scripts/CarControl.cs
+++ USB SEX ROBOT/Assets/Scripts/CarControl.cs
@@ -29,7 +29,13 @@ public class CarControl : MonoBehaviour {
     float steeringWheelSpeed = 20.0f;
 
     [SerializeField]
-    Vector2 SteeringWheelClip = new Vector2(-20.0f, 20.0f);
+    float SteeringWheelClip = 90.0f;
+
+    [SerializeField]
+    float sideLookClip = 30.0f;
+
+    [SerializeField]
+    float maxLookSpeed = 150.0f;
 
     [SerializeField][Tooltip ("accel, decel, idle, 3x tyres")]
     AudioClip[] allCarSounds;
@@ -43,6 +49,10 @@ public class CarControl : MonoBehaviour {
 
     bool driftSoundReady = true;
 
+    Vector3 startLookAngle;
+
+    float oldYangle = 0.0f;
+
 
     // Use this for initialization
     void Start () {
@@ -51,6 +61,8 @@ public class CarControl : MonoBehaviour {
 
         GetComponent<AudioSource>().clip = allCarSounds[2];
         GetComponent<AudioSource>().Play();
+
+        startLookAngle = Camera.main.transform.localEulerAngles;
 
     }
 	
@@ -62,8 +74,7 @@ public class CarControl : MonoBehaviour {
 
         Vector3 localVelocity = transform.InverseTransformDirection(GetComponent<Rigidbody>().velocity);
 
-        //Debug.Log(localVelocity.z);
-
+        //check how long the car not moving forward.
         if (localVelocity.z < 0.1f)
         {
             TimeNotMovingForward += Time.fixedDeltaTime;
@@ -74,8 +85,7 @@ public class CarControl : MonoBehaviour {
 
 
 
-        //Field of view adjustment for extra sppeeeed.
-        //float percentageFOV = GetComponent<Rigidbody>().velocity.magnitude / maxSpeed;
+        //Field of view adjustment for extra sppeeeed.        
         float percentageFOV = 0; 
 
         if (GetComponent<Rigidbody>().velocity.magnitude > maxSpeed / 4)
@@ -109,12 +119,12 @@ public class CarControl : MonoBehaviour {
 
 
             //steering wheel turn
-            if (rotationQuantity.y > 0 && SteeringWheelRotation > SteeringWheelClip.y)
+            if (rotationQuantity.y > 0 && SteeringWheelRotation > -SteeringWheelClip)
             {
                 SteeringWHeel.transform.Rotate(0, 0, -rotationQuantity.y * steeringWheelSpeed * Time.deltaTime);
                 SteeringWheelRotation += -rotationQuantity.y * steeringWheelSpeed * Time.deltaTime;
             }
-            else if (rotationQuantity.y < 0 && SteeringWheelRotation < SteeringWheelClip.x)
+            else if (rotationQuantity.y < 0 && SteeringWheelRotation < SteeringWheelClip)
             {
                 SteeringWHeel.transform.Rotate(0, 0, -rotationQuantity.y * steeringWheelSpeed * Time.deltaTime);
                 SteeringWheelRotation += -rotationQuantity.y * steeringWheelSpeed * Time.deltaTime;
@@ -124,6 +134,8 @@ public class CarControl : MonoBehaviour {
         }
         else
         {
+            // when not steering, set steeringwheel back to neutral position.
+
             if (SteeringWheelRotation > 1.0f)
             {
                 SteeringWHeel.transform.Rotate(0, 0, -steeringWheelSpeed * Time.deltaTime);
@@ -137,13 +149,7 @@ public class CarControl : MonoBehaviour {
             }
         }
 
-     //   else if (SteeringWHeel.transform.rotation.z > 0.2f)
-     //       SteeringWHeel.transform.Rotate(0, 0, steeringWheelSpeed * Time.deltaTime);
-     //   else if (SteeringWHeel.transform.rotation.z < 0.2f)
-     //       SteeringWHeel.transform.Rotate(0, 0,  steeringWheelSpeed * Time.deltaTime);
-
-
-
+    
         //dont tip the car
 
         if (toRotation.eulerAngles.x > 0.01f || toRotation.eulerAngles.z > 0.01f)
@@ -205,27 +211,13 @@ public class CarControl : MonoBehaviour {
             //brake when having forwardspeed, speed at all (to keep sliding)
             if (localVelocity.z > 0.1f)
             {
+                //start drift sounds, make sure it doesnt start again.
                 if (driftSoundReady == true && GetComponent<Rigidbody>().velocity.magnitude > maxBreakSpeed/2)
                 {
                     GetComponent<AudioSource>().PlayOneShot(allCarSounds[4]);
                     driftSoundReady = false;
                 }
-                /*
-                if (GetComponent<AudioSource>().clip != allCarSounds[4] && GetComponent<AudioSource>().clip != allCarSounds[2])
-                {
-                    GetComponent<AudioSource>().Stop();
-                    GetComponent<AudioSource>().clip = allCarSounds[4];
-                    GetComponent<AudioSource>().PlayOneShot(allCarSounds[4]);
-                }
-                else if (GetComponent<AudioSource>().clip != allCarSounds[2] && !GetComponent<AudioSource>().isPlaying)
-                {
-                    GetComponent<AudioSource>().Stop();
-                    GetComponent<AudioSource>().clip = allCarSounds[2];
-                    GetComponent<AudioSource>().Play();
-                }*/
-
-
-
+                
                 GetComponent<Rigidbody>().AddRelativeForce(-Vector3.forward * maxBreakSpeed * Input.GetAxis("LeftBump") * Time.fixedDeltaTime);
             }
             else if (TimeNotMovingForward > 0.4f && GetComponent<Rigidbody>().velocity.magnitude <= maxSpeed/2)
@@ -235,24 +227,25 @@ public class CarControl : MonoBehaviour {
 
         }
 
+        //ready for next drift sound if break is released
         if (Input.GetAxis("LeftBump") < 0.05f && driftSoundReady == false)
             driftSoundReady = true;
 
+        //look sideways
+        float newYAngle = Input.GetAxis("RightX") * sideLookClip + startLookAngle.y;
+
+        if (newYAngle > oldYangle + maxLookSpeed * Time.deltaTime)
+            newYAngle = oldYangle + maxLookSpeed * Time.deltaTime;
+
+        if (newYAngle < oldYangle - maxLookSpeed * Time.deltaTime)
+            newYAngle = oldYangle - maxLookSpeed * Time.deltaTime;
+
+        
+        Camera.main.transform.localRotation = Quaternion.Euler(startLookAngle.x, newYAngle, startLookAngle.z);
+        oldYangle = newYAngle;
 
     }
     
 
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.black;
-        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 10);
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(transform.position, transform.position + GetComponent<Rigidbody>().velocity);
-
-        Vector3 reflection = -Vector3.ProjectOnPlane(GetComponent<Rigidbody>().velocity, transform.forward);
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, transform.position + reflection);
-    }
+    
 }
